a7114fc64d94cf134dec58fbec06c04c2f9df74a
justlive1 <qq11419041@163.com>
2018-07-16 22:55:11 +0800

    用户controller

---
+++ helium-httpserver/src/main/java/vip/justlive/helium/httpserver/controller/UserController.java
@@ -0,0 +1,18 @@
+package vip.justlive.helium.httpserver.controller;
+
+import vip.justlive.common.web.vertx.annotation.VertxRoute;
+import vip.justlive.common.web.vertx.annotation.VertxRouteMapping;
+
+/**
+ * 用户路由
+ *
+ * @author wubo
+ */
+@VertxRoute
+public class UserController {
+
+  @VertxRouteMapping("/register")
+  public void register() {
+
+  }
+}
